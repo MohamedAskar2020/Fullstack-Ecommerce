c756d947f3e9d372f37eface68f73790207b871c
MohamedAskar2020 <dr.moh.t.askar@gmail.com>
2024-11-19 22:43:38 +0300

    Drizzle setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,19 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import productsRoutes from "./routes/products/index";
 
 const app = express();
-
 const port = 3000;
+
+// Middleware:
+app.use(urlencoded({ extended: false }));
+app.use(json());
+
+// Routes:
+app.use("/products", productsRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
-
-app.use("/products", productsRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
